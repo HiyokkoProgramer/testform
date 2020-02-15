25346aae051a1ec3118baf7c7b135d44f4aea1fd
HiyokkoProgramer <blogkankei.senyou@gmail.com>
2020-02-15 04:28:36 +0000

    foreachディレクティブの例

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+
+    public function index(){
+        $data=['one','two','three','four','five'];
+        return view('hello.index',['data'=>$data]);
+    }
+
+    public function post(Request $request){
+        $name= $request->name;
+
+        return view('hello.index',['name'=>$request->name]);
+    }
+
+}
